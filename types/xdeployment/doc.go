1e10768e97ee0802e5728c87135ae63bff8d193e
Reza Karamad <r.karamad@gmail.com>
2026-06-15 20:57:17 +0200

    feat: add types/xtenant module

---
+++ types/xdeployment/doc.go
@@ -0,0 +1,7 @@
+// Package xdeployment contains the Go types for the XDeployment Composite Resource Definition (XRD).
+//
+// These types are the single source of truth for the XDeployment XR shape.
+// Use cmd/gen-xrd to generate the XRD YAML from this package.
+//
+// +groupName=idp.rezakara.demo
+package xdeployment
